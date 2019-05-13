f6743a5ef788bfc7a8b459e8605a00d73d42ee84
Naina Vig <naina.r.vig@gmail.com>
2019-05-13 23:58:53 +0000

    MONDAY

--- querier/querier.c
+++ querier/querier.c
@@ -18,19 +18,26 @@
 /*************************** union and intersection utilities ************************/
 
 
-// used to get intersection of counters
+// wraps two counters in struct to pass to iterator helper function
 typedef struct two_cntrs{
 	counters_t *c1;
 	counters_t *empty;
 } two_cntrs_t;
 
-// used to get min count for intersection
-void iter_func(void *arg, const int key, const int count){
+// helper function for counters_intersect
+// for each (key, count) in counters, search c1 for key
+// if key in c1, add the key and the minimum of the two counters to counters
+void inter_helper(void *arg, const int key, const int count){
+
+	//initialize variables
 	two_cntrs_t *t = arg;
 	counters_t *c1 = t->c1;
 	counters_t *empty = t->empty;
 	int count1;
+
+	// check if key is in c1
 	if ((count1 = counters_get(c1, key)) != 0){
+		// set key to the minimum of two counts in new counters set
 		if (count < count1){
 			counters_set(empty, key, count);
 		} else {
@@ -40,25 +47,28 @@ void iter_func(void *arg, const int key, const int count){
 }
 
 // computes counters intersection
-counters_t *counters_intersect(counters_t *counters, counters_t *new){
+counters_t *counters_intersect(counters_t *counters, counters_t *c_word){
+	// create new counters set for intersection
 	counters_t *new_intersection = counters_new();
+
+	// create new two_cntrs struct to hold new intersection and word counters
 	two_cntrs_t *t = malloc(sizeof(two_cntrs_t));
-	t->c1 = new;
+	t->c1 = c_word;
 	t->empty = new_intersection;
-	counters_iterate(counters, t, iter_func);
+	counters_iterate(counters, t, inter_helper);
 	free(t);
 	return new_intersection;
 }
 
 // used for computing counters union
-void union_func(void *arg, const int key, const int count){
+void union_helper(void *arg, const int key, const int count){
 	int i = counters_get(arg, key);
 	counters_set(arg, key, i+count);
 }
 
 // used to take the union of two counters
 void counters_union(counters_t *counters, counters_t *new){
-	counters_iterate(new, counters, union_func);
+	counters_iterate(new, counters, union_helper);
 }
 
 // used in debugging for printing hashtable
@@ -97,10 +107,10 @@ typedef struct sort_node {
 
 
 // used to store array and index info as arg to set iterate
-typedef struct list {
+typedef struct sortedlist {
 	sort_node_t ** arr;
 	int *i;
-} list_t;
+} sortedlist_t;
 
 // called by counters_iterate, helps get size of counters
 void get_size(void *arg, const int key, const int count){
@@ -111,23 +121,24 @@ void get_size(void *arg, const int key, const int count){
 void sort_helper(void *arg, const int key, const int count){
 
 	// create new node to add to list
-	sort_node_t *s = malloc(sizeof(sort_node_t));
-	s->key = key;
-	s->count = count;
-
+	sort_node_t *node = malloc(sizeof(sort_node_t));
+	node->key = key;
+	node->count = count;
 
 	// get info from arg
-	list_t *t = arg;
-	sort_node_t ** arr = t->arr;
-	int *i = t->i;
+	sortedlist_t *list = arg;
+	sort_node_t ** arr = list->arr; // list
+	int *i = list->i;	// index for list 
 
-	// insert into sorted list
+	// find index for node
 	int j = (*i)-1;
 	while (j >= 0 && arr[j]->count > count){
 		arr[j + 1] = arr[j];
 		j = j - 1;
 	}
-	arr[j+1] = s;
+
+	// place node in list
+	arr[j+1] = node;
 
 	// increment index
 	(*i) = (*i) + 1;
@@ -135,45 +146,38 @@ void sort_helper(void *arg, const int key, const int count){
 
 
 // sorts counters into array of (key, count) structs
-list_t *
-sort_counters(counters_t *counters){
+void
+sort_and_display(counters_t *counters){
+
+	// get length of counters list
 	int c_size = 0;
 	counters_iterate(counters, &c_size, get_size);
 
-	sort_node_t ** arr = malloc(sizeof(sort_node_t)*c_size);
-
-	list_t *l = malloc(sizeof(list_t));
-	l->arr = arr;
-	int j = 0;
-	l->i = &j; 
-
-	counters_iterate(counters, l, sort_helper);
-
-	return l;
-}
-
-void 
-display_results(list_t *l, char *pageDirectory){
-	sort_node_t ** arr = l->arr;
-	int *length = l->i;
-
-	for (int i = 0; i < *length; i++){
-		printf("%d: %d", arr[i]->key, arr[i]->count);
+	// if counters list is empty, don't print anything
+	if (c_size == 0){
+		return;
 	}
+
+	// initialize array
+	sort_node_t ** arr = calloc(c_size, sizeof(sort_node_t));
+
+	// store array in list struct and set list struct index to 0
+	sortedlist_t *list = malloc(sizeof(sortedlist_t));
+	list->arr = arr;
+	int j = 0;
+	list->i = &j; 
+
+	// fill sorted array
+	counters_iterate(counters, list, sort_helper);
+
+	// loop through array and print elements
+	int k;
+	for (k = 0; k < *(list->i); k++){
+		printf("document %d: %d\n", arr[k]->key, arr[k]->count);
+	}
+	return;
 }
 
-void duplicate_helper(void *arg, const int key, const int count){
-	counters_t *counters = arg;
-	counters_set(counters, key, count);
-}
-
-counters_t *
-duplicate_counters(counters_t *counters){
-	counters_t *new_counters = counters_new();
-
-	counters_iterate(counters, new_counters, duplicate_helper);
-	return new_counters;
-}
 
 
 /*************************** main algorithm ***************************************/
@@ -227,9 +231,6 @@ parse_input(char *line){
 		i++;	
 	}
 
-	for (int j = 0; j < i; j++){
-		printf("%s\n", arr[j]);
-	}
 		
 	s->arr = arr;
 	s->num_words = i;
@@ -243,6 +244,7 @@ querier(hashtable_t *index, parsed_line_t *s){
 	// maintains whether conjunction is last word seen
 	bool conj_last_seen = false;
 
+	// intialize union and intersect counters sets
 	counters_t *c_union = counters_new();
 	counters_t *c_intersect = NULL;
 
@@ -276,31 +278,50 @@ querier(hashtable_t *index, parsed_line_t *s){
 			if (counters != NULL){
 				// if intersection uninitialized, set to counters
 				if (c_intersect == NULL){
-					c_intersect = duplicate_counters(counters);
+					//c_intersect = duplicate_counters(counters);
+					c_intersect = counters_new();
+					counters_union(c_intersect, counters);
 				// otherwise, take intersection of counters with current intersect counters set
 				} else {
 					counters_t *new_intersection = counters_intersect(c_intersect, counters);
+					counters_delete(c_intersect);
 					c_intersect = new_intersection;
 				}
 			}
 			conj_last_seen = false;	
 		}
 	}
+	
+	if (conj_last_seen){
+		printf("Error: cannot end query with conjunction\n");
+		return NULL;	
+	}
+
 	if (c_intersect != NULL){	
 		counters_union(c_union, c_intersect);
 		counters_delete(c_intersect);
 	}
-	counters_print(c_union, stdout);
 	return c_union;
 }
 
 
 
 int main(int argc, char **argv){
+
+	// verify number of arguments
 	if (argc != 3){
 		printf("usage: ./querier pageDirectory indexFilename\n"); 
+		return 1;
 	}
+
+	// make sure index file can be opened
 	FILE *fp = fopen(argv[2], "r");
+	if (fp == NULL){
+		printf("Error: indexFilename cannot be opened\n");
+		return 1;
+	}
+
+	// load file into index
 	hashtable_t *index = indexLoad(fp);
 
 //	hashtable_print(index, stdout, print_item);
@@ -330,13 +351,10 @@ int main(int argc, char **argv){
 	while (fgets(line, 50, stdin) != NULL){
 		parsed_line_t *s  = parse_input(line);
 
-
 		counters_t *result = querier(index, s);
-	//	sort_counters(result);
-
-	//	if (result != NULL){
-	//		display_results(result, argv[1]);
-	//	}
+		
+		sort_and_display(result);
+		
 		counters_delete(result);
 		printf("Query? ");
 	
